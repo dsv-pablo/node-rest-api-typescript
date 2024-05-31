b0b9280c36fda4827f956105b9d474e45f752814
Pablo Vieira <63684077+dsv-pablo@users.noreply.github.com>
2024-05-31 21:24:32 +0000

    feat: Added multiple errors validation suport and yup translations

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import "dotenv/config";
+
+import "./shared/services/YupTranslations";
 import { router } from "./routes";
 
 const server = express();
--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
 
 interface ICidade {
@@ -7,21 +8,25 @@ interface ICidade {
 
 const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
   nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
 });
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   let validatedData: ICidade | undefined;
+  let errors: Record<string, string> = {};
 
   try {
-    validatedData = await bodyValidation.validate(req.body);
-  } catch (error) {
-    const yupError = error as yup.ValidationError;
-
-    return res.json({
-      errors: {
-        default: yupError.message,
-      },
+    validatedData = await bodyValidation.validate(req.body, {
+      abortEarly: false,
     });
+  } catch (err) {
+    const yupError = err as yup.ValidationError;
+    yupError.inner.map((error) => {
+      if (!error.path) return;
+      errors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({ errors });
   }
   return res.send(`O nome enviado foi ${req.body.nome}`);
 };
